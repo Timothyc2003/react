c03279665a48b86abad3046cee2cc81999f53b41
Timothy Chung <timothychung@Timothys-MacBook-Pro.local>
2025-03-31 23:24:23 -0400

    second commit

---
+++ app/components/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Enable if using static export (uncomment if needed)
+    // output: 'export', // Only for fully static sites (no API routes)
+  };
+  
+  module.exports = nextConfig;
